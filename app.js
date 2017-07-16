5c9c96813cabe50419682d636894e70894bfbb8e
Ben Hubsch <bah37@duke.edu>
2017-07-15 20:07:53 -0700

    working express

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+
+var exphbs = require('express-handlebars')
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/about', function(req, res) {
+    res.render('about')
+})
+
+app.get('/contact', function(req, res) {
+    res.render('contact')
+})
+
+app.get('/campus', function(req, res) {
+    res.render('campus')
+})
+
+app.get('/projects', function(req, res) {
+    res.render('projects')
+})
+
+app.get('/', function(req, res) {
+    res.render('home')
+})
+
+// app.get('*',function (req, res) {
+//     res.redirect('/');
+// });
+
+var port = process.env.PORT || 3000
+app.listen(port, function() {
+    console.log('succesfully connected to port', port);
+})
